741bf5895d309c1411770ebbd78ed38f2039dee3
Denis Gutierrez <infotech.ench.sho28@gmail.com>
2017-07-02 21:49:10 +0800

    q

---
+++ backend/application/controllers/Customers.php
@@ -0,0 +1,283 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+class Customers extends CORE_Controller
+{
+    function __construct()
+    {
+        
+        //$this->custom_token();
+        
+        parent::__construct('');
+        $this->load->model(array(
+            'Customer_model'
+        ));
+        
+    }
+    
+
+    
+    public function transaction($txn = null)
+    {
+        switch ($txn) {
+            case 'list':
+                $response['data'] = $this->get_response_rows();
+                echo json_encode($response);
+                break;
+            
+            case 'create':
+                $m_cust_account= $this->Customer_model;
+                $cust_email     = $this->input->post('cust_email', TRUE);
+                if ($this->input->post('cust_email') == null) {
+                    $response['title'] = 'Error!';
+                    $response['stat']  = 'error';
+                    $response['msg']   = 'Sorry, Invalid email address!';
+                    echo json_encode($response);
+                    exit;
+                }
+                
+                if ($this->input->post('cust_pword') == null) {
+                    $response['title'] = 'Error!';
+                    $response['stat']  = 'error';
+                    $response['msg']   = 'Sorry, Invalid  password!';
+                    echo json_encode($response);
+                    exit;
+                }
+                
+                if ($this->input->post('cust_cpword') != $this->input->post('cust_pword')) {
+                    $response['title'] = 'Error!';
+                    $response['stat']  = 'error';
+                    $response['msg']   = 'Sorry, Password did not match!';
+                    echo json_encode($response);
+                    exit;
+                }
+                
+                // validate if cust_email is already registered
+                
+                $cust_email_exists = $m_cust_account->get_list(array(
+                    'cust_accounts.cust_email' => $cust_email
+                ));
+                if (count($cust_email_exists) > 0) {
+                    $response['title'] = 'Error!';
+                    $response['stat']  = 'error';
+                    $response['msg']   = 'Sorry, This email is already in use.';
+                    echo json_encode($response);
+                    exit;
+                }
+                
+                // validate if custname is already registered
+                
+                $cust_uname        = $this->input->post('cust_uname', TRUE);
+                $cust_uname_exists = $m_cust_account->get_list(array(
+                    'customers.cust_uname' => $cust_uname
+                ));
+                if (count($cust_uname_exists) > 0) {
+                    $response['title'] = 'Error!';
+                    $response['stat']  = 'error';
+                    $response['msg']   = 'Sorry, This custname is already in use.';
+                    echo json_encode($response);
+                    exit;
+                }
+                
+                $m_cust_account->begin();
+                $m_cust_account->cust_uname = $this->input->post('cust_uname', TRUE);
+                $m_cust_account->cust_pword = sha1($this->input->post('cust_pword', TRUE));
+                $m_cust_account->cust_email = $this->input->post('cust_email', TRUE);
+                $m_cust_account->cust_fname = $this->input->post('cust_fname', TRUE);
+                $m_cust_account->cust_lname = $this->input->post('cust_lname', TRUE);
+                $m_cust_account->cust_mname = $this->input->post('cust_mname', TRUE);
+                $m_cust_account->contact_no = $this->input->post('contact_no', TRUE);
+                $m_cust_account->address    = $this->input->post('address', TRUE);
+                $m_cust_account->cust_bdate = date('Y-m-d', strtotime($this->input->post('cust_bdate', TRUE)));
+                
+                // auditing purposes
+                
+                $m_cust_account->save();
+                $cust_id = $m_cust_account->last_insert_id();
+                $m_cust_account->commit();
+                if ($m_cust_account->status() === TRUE) {
+                    $response['title']     = 'Success!';
+                    $response['stat']      = 'success';
+                    $response['msg']       = 'cust successfully registered.';
+                    $response['row_added'] = $this->get_response_rows($cust_id);
+                } else {
+                    $response['title'] = 'Error!';
+                    $response['stat']  = 'error';
+                    $response['msg']   = 'Something went wrong! Please try again.';
+                }
+                
+                echo json_encode($response);
+                break;
+            
+            case 'update':
+                $m_cust_account = $this->Customer_model;
+                $cust_id        = $this->input->post('cust_account_id', TRUE);
+                if ($this->input->post('cust_pword') != null) { //if password is provide, cust wanted to update the password so it must be validated
+                    if ($this->input->post('cust_cpword') != $this->input->post('cust_pword')) {
+                        $response['title'] = 'Error!';
+                        $response['stat']  = 'error';
+                        $response['msg']   = 'Sorry, Password did not match!';
+                        echo json_encode($response);
+                        exit;
+                    }
+                }
+                
+                $m_cust_account->begin();
+                $m_cust_account->cust_uname = $this->input->post('cust_uname', TRUE);
+                if ($this->input->post('cust_pword') != null) { //if not provided, do not updated password
+                    $m_cust_account->cust_pword = sha1($this->input->post('cust_pword', TRUE));
+                }
+                
+                $m_cust_account->cust_uname = $this->input->post('cust_uname', TRUE);
+                $m_cust_account->cust_pword = sha1($this->input->post('cust_pword', TRUE));
+                $m_cust_account->cust_email = $this->input->post('cust_email', TRUE);
+                $m_cust_account->cust_fname = $this->input->post('cust_fname', TRUE);
+                $m_cust_account->cust_lname = $this->input->post('cust_lname', TRUE);
+                $m_cust_account->cust_mname = $this->input->post('cust_mname', TRUE);
+                $m_cust_account->contact_no = $this->input->post('contact_no', TRUE);
+                $m_cust_account->address    = $this->input->post('address', TRUE);
+                $m_cust_account->cust_bdate = date('Y-m-d', strtotime($this->input->post('cust_bdate', TRUE)));
+                
+                // auditing purposes
+                
+                $m_cust_account->modify($cust_id);
+                $m_cust_account->commit();
+                if ($m_cust_account->status() === TRUE) {
+                    $response['title']       = 'Success!';
+                    $response['stat']        = 'success';
+                    $response['msg']         = 'cust information successfully updated.';
+                    $response['row_updated'] = $this->get_response_rows($cust_id);
+                } else {
+                    $response['title'] = 'Error!';
+                    $response['stat']  = 'error';
+                    $response['msg']   = 'Something went wrong! Please try again.';
+                }
+                
+                echo json_encode($response);
+                break;
+            
+            case 'delete':
+                $m_cust_account = $this->Customer_model;
+                $cust_id        = $this->input->post('cust_account_id', TRUE);
+                $m_cust_account->begin();
+                $m_cust_account->is_deleted = 1;
+                
+                $m_cust_account->modify($cust_id);
+                
+                // make sure to update status of the cust
+                
+                $m_cust_account->is_active = 0;
+                $m_cust_account->modify($cust_id);
+                $m_cust_account->commit();
+                if ($m_cust_account->status() === TRUE) {
+                    $response['title'] = 'Success!';
+                    $response['stat']  = 'success';
+                    $response['msg']   = 'cust information successfully deleted.';
+                } else {
+                    $response['title'] = 'Error!';
+                    $response['stat']  = 'error';
+                    $response['msg']   = 'Something went wrong. Please try again later.';
+                }
+                
+                echo json_encode($response);
+                break;
+            
+            case 'upload':
+                $allowed   = array(
+                    'png',
+                    'jpg',
+                    'jpeg',
+                    'bmp'
+                );
+                $data      = array();
+                $files     = array();
+                $directory = 'assets/images/cust/';
+                foreach ($_FILES as $file) {
+                    $server_file_name = uniqid('');
+                    $extension        = pathinfo($file['name'], PATHINFO_EXTENSION);
+                    $file_path        = $directory . $server_file_name . '.' . $extension;
+                    $orig_file_name   = $file['name'];
+                    if (!in_array(strtolower($extension), $allowed)) {
+                        $response['title'] = 'Invalid!';
+                        $response['stat']  = 'error';
+                        $response['msg']   = 'Image is invalid. Please select a valid photo!';
+                        die(json_encode($response));
+                    }
+                    
+                    if (move_uploaded_file($file['tmp_name'], $file_path)) {
+                        $response['title'] = 'Success!';
+                        $response['stat']  = 'success';
+                        $response['msg']   = 'Image successfully uploaded.';
+                        $response['path']  = $file_path;
+                        echo json_encode($response);
+                    }
+                }
+                
+                break;
+            
+            case 'update-profile':
+                $m_custs                    = $this->Customer_model;
+                $cust_account_id            = 1; // Active cust
+                $m_cust_account->cust_uname = $this->input->post('cust_uname', TRUE);
+                if ($this->input->post('cust_pword') != null) { //if not provided, do not updated password
+                    $m_cust_account->cust_pword = sha1($this->input->post('cust_pword', TRUE));
+                }
+                
+                $m_cust_account->cust_uname = $this->input->post('cust_uname', TRUE);
+                $m_cust_account->cust_pword = sha1($this->input->post('cust_pword', TRUE));
+                $m_cust_account->cust_email = $this->input->post('cust_email', TRUE);
+                $m_cust_account->cust_fname = $this->input->post('cust_fname', TRUE);
+                $m_cust_account->cust_lname = $this->input->post('cust_lname', TRUE);
+                $m_cust_account->cust_mname = $this->input->post('cust_mname', TRUE);
+                $m_cust_account->contact_no = $this->input->post('contact_no', TRUE);
+                $m_cust_account->address    = $this->input->post('address', TRUE);
+                $m_cust_account->cust_bdate = date('Y-m-d', strtotime($this->input->post('cust_bdate', TRUE)));
+                $m_custs->cust_photo        = $this->input->post('cust_photo');
+                
+                $m_custs->modify($cust_account_id);
+                $response['title'] = 'Success!';
+                $response['stat']  = 'success';
+                $response['msg']   = 'Profile successfully updated.';
+                echo json_encode($response);
+                break;
+        }
+    }
+    
+    private function get_response_rows($id = null)
+    {
+        $m_cust_account = $this->Customer_model;
+        return $m_cust_account->get_list();
+    }
+    
+    
+    public function auth_cust()
+    {
+        $cust_uname = $this->input->post('cust_uname');
+        $cust_pword = sha1($this->input->post('cust_pword'));
+        
+        $m_cust_account = $this->Customer_model;
+        $result         = $m_cust_account->get_list(array(
+            'customers.cust_uname' => $cust_uname,
+            'customers.cust_pword' => $cust_pword
+        ));
+        
+        if (count($result) > 0) { //valid custname and pword
+            
+            
+            $response['title'] = 'Success';
+            $response['stat']  = 'success';
+            $response['msg']   = 'Login successfully';
+            
+            echo json_encode($response);
+        } else {
+            
+            
+            
+            $response['title'] = 'Failed';
+            $response['stat']  = 'error';
+            $response['msg']   = 'Login not successful!';
+            echo json_encode($response);
+        }
+    }
+    
+    
+}
